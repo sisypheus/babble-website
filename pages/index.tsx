92ff0b21a82233b642202cc31775a93a3ab8b980
Theo <poette.theo@gmail.com>
2022-07-17 01:09:00 +0200

    fix: build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header/Header";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
