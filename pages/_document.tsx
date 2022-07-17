07d759594138a698bd5b52ff1a9ef97324a1ba8d
Theo <poette.theo@gmail.com>
2022-07-18 00:24:21 +0200

    fix: FOUC in prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
 import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-const getInitialProps = createGetInitialProps();
-
 export default class _Document extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const mantine = createGetInitialProps(); 
+    return { ...initialProps, ...mantine };
   }
   render() {
     return (
