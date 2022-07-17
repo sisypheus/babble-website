d2e526d21821ae22a23cce573f6f20d0b0072b3a
Theo <poette.theo@gmail.com>
2022-07-18 00:22:03 +0200

    fix: FOUC in prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,16 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
   render() {
     return (
       <Html>
         <Head />
         <body>
+          <script>0</script>
           <Main />
           <NextScript />
         </body>
