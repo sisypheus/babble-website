c2ff1fc680956012c505ecff4a477a8ef960958d
Theo <poette.theo@gmail.com>
2022-07-18 22:11:29 +0200

    feat: testing fouc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,10 @@
-import {
-  ServerStyles,
-  createStylesServer,
-} from "@mantine/next";
+import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-const stylesServer = createStylesServer();
+const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static async getInitialProps(ctx: any) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles
-          html={initialProps.html}
-          server={stylesServer}
-          key="styles"
-        />,
-      ],
-    };
-  }
+  static getInitialProps = getInitialProps;
 
   render() {
     return (
