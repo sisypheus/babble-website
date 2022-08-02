583cbbae6a2246f7f456aafbf0ba05e5624ea039
Theo <poette.theo@gmail.com>
2022-08-02 23:52:17 +0200

    feat: use of latest widget possible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App(props: AppProps) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
                   __html: `
-                (function (w, d, s, o, f, js, fjs) { w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) }; js = d.createElement(s), fjs = d.getElementsByTagName(s)[0]; js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs); }(window, document, 'script', 'aw1', 'https://cdn.jsdelivr.net/gh/sisypheus/babble-widget-release@latest/widget.js')); aw1('init', { clientId: '332ee05c-59f1-4d0c-b26a-375c8be9ae9e', widget: { color: "#3b81f6", title: "Welcome to Babble", subtitle: "Chat with us here", minimized: true, } } );
+                (function (w, d, s, o, f, js, fjs) { w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) }; js = d.createElement(s), fjs = d.getElementsByTagName(s)[0]; js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs); }(window, document, 'script', 'aw1', 'https://cdn.jsdelivr.net/gh/sisypheus/babble-widget-release@latest/widget.min.js')); aw1('init', { clientId: '332ee05c-59f1-4d0c-b26a-375c8be9ae9e', widget: { color: "#3b81f6", title: "Welcome to Babble", subtitle: "Chat with us here", minimized: true, } } );
         `,
         }} 
       />
