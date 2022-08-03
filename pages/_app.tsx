a7bc90ebba5701727311f923a74339a8294cf65b
Theo <poette.theo@gmail.com>
2022-08-03 23:40:34 +0200

    feat: correct client id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,9 @@ export default function App(props: AppProps) {
         id="babble-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-                  __html: `
-                (function (w, d, s, o, f, js, fjs) { w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) }; js = d.createElement(s), fjs = d.getElementsByTagName(s)[0]; js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs); }(window, document, 'script', 'aw1', 'https://cdn.jsdelivr.net/gh/sisypheus/babble-widget-release@latest/widget.min.js')); aw1('init', { clientId: '332ee05c-59f1-4d0c-b26a-375c8be9ae9e', widget: { color: "#3b81f6", title: "Welcome to Babble", subtitle: "Chat with us here", minimized: true, } } );
-        `,
-        }} 
+          __html: `
+(function (w, d, s, o, f, js, fjs) { w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) }; js = d.createElement(s), fjs = d.getElementsByTagName(s)[0]; js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs); }(window, document, 'script', 'aw1', 'https://cdn.jsdelivr.net/gh/sisypheus/babble-widget-release@latest/widget.min.js')); aw1('init', { clientId: 'fbe42fe7-9d45-4ae8-989d-fa25386f771d', widget: { color: "#3b81f6", title: "Welcome to The startup 2", subtitle: "Chat with us", minimized: true, } } );        `,
+        }}
       />
 
       <MantineProvider withGlobalStyles withNormalizeCSS>
